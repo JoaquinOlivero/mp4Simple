31dec05855f09f94cca6dd2333322762a90f4997
JoaquinOlivero <joaquinolivero97@gmail.com>
2022-05-29 02:33:37 -0300

    body object fix

--- sonarr.py
+++ sonarr.py
@@ -4,7 +4,7 @@ def processEpisode(dirName, settings, logging):
     logging.info("Sending request to Sonarr api")
     try:
         url = "http://" + settings["url"] + ":" + str(settings["port"]) + "/api/v3/command"
-        payload = {'name': 'DownloadedMoviesScan', 'path': settings["download_dir"] + dirName}
+        payload = {'name': 'DownloadedEpisodesScan', 'path': settings["download_dir"] + dirName}
         headers = {
             'X-Api-Key': settings["api_key"]
         }
